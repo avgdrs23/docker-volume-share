bd5d7f9e65a69e9eceff1e3061894652b682e529
avishay <avgdrs23@gmail.com>
2021-12-15 20:24:43 +0200

    first commit

---
+++ writingfile1.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+import time
+
+with open("data/D1.txt","w") as f:
+   i=0
+   while True:
+      print(f"writing from docker   {i}...")
+      f.write(f"Writing from docker  {i}\n ")
+      f.flush()
+      time.sleep(1)
+      i=i+1
